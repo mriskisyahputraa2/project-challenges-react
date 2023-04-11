d3a9c695e7c722e4a4fa2861132d8e19cb91dfa5
mriskisyahputraa2 <riskideveloper2@gmail.com>
2023-04-12 00:38:55 +0700

    add style todolist

--- to-do-list/src/App.js
+++ to-do-list/src/App.js
@@ -1,8 +1,85 @@
+import './App.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { 
+  faCircleCheck, faPen, faTrashCan
+} from '@fortawesome/react-fontawesome';
+import React, { useState } from 'react';
 
 function App() {
+
+  // task (todo list) state
+  const [todo, setTodo] = useState([
+    {"id":1, "title": "Task 1", "status": true}, // jika true berarti tugas selesai dan jika false sebaliknya
+    {"id":2, "title": "Task 2", "status": false},
+  ]);
+
+
+  // temp task / tugas data sementara 
+  const [newTask, setNewTask] = useState(''); // digunakan untuk menyimpan data sementara yang ditambahkan user
+  const [updateData, setUpdateData] = useState(''); // digunakan untuk menahan data yang sedang di update 
+
+
+  // Add Task / menambahkan tugas
+  const addTask = () => {
+    
+  } 
+
+
+  // Delete Task / Hapus Data
+  const deleteTask = (id) => {
+    
+  }
+  
+  
+  // Mark task as done or completed / menandai tugas sudah complit
+  const completedTask = (id) => {
+
+  }
+
+
+  // Cancel Update
+  const cancelUpdate = () => {
+
+  }
+
+  // Change task for Update 
+  const changeTask = (e) => {
+
+  }
+
+  // Update task
+  const UpdateTask = () => {
+
+  }
+
   return (
     <>
-      <h1 className="text-green-500">Hello Word</h1>
+      <div className='App'>
+        <h1 className='text-center text-3xl font-roboto font-medium text-white my-12'>To Do List</h1>
+
+
+      {/* Display Todos */}
+      </div>
+        {todo && todo.length ? '' : <p className='text-center text-green-500 text-bold'>No Task....</p>}
+      <div>
+        <div>
+          {todo && todo.map((task, index) => {
+            return (
+              <React.Fragment key={task.id}>
+                  <div className='flex flex-wrap bg-emerald-700 shadow-lg shadow-indigo-500/40 rounded-xl mb-3 px-4 py-3 text-left text-lg relative mx-auto max-w-sm'>
+                    {/* jika status nya sama dengan done maka coret text nya */}
+                    <div className={task.status ? 'done' : ''}>
+                      <span className=' inline-block border-solid border-2 rounded-full border-slate-300 w-[25px] h-[25px] text-white text-center mr-[8px] mb-2 text-sm'>{index + 1}</span>
+                      <span className='taskText text-white '>{task.title}</span>
+                    </div>
+                  </div>
+
+              </React.Fragment>
+            )
+          })}
+        </div>
+      </div>
+
     </>
   );
 }
